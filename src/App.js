9f91c6ce0f770281f2830b1ef838b07fd6599bbb
soyeya <parksoye123@gmail.com>
2023-01-02 20:11:16 +0900

    토글기능_상태변수

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 
 import{ useState } from 'react'; //내장되어있는 경우 {}를 사용해서 부름 (이미 리액트안에 내장되어있음 _node_modules안에 포함)
+import './App.css';
 
 //비동기의 경우에도 useState가 있어야만 사용가능_ 시간을 맞추기가 어려움
+
+
 
 function App() {
 
   //useState로 선언된 변수의 값이 달라지면
   //App이라는 컴포넌트를 재랜더링한다.
   
-  const [count, setCount] = useState(0);
+  const [open, setToggle] = useState(true);
   //count는 변수명
   //count의 값은 useState(_)안의 값, setCount는 값을 변하게 대입할 메소드.
   return (
 
-
-    <div className="App"> 
+    <div className={ open ? "white" : "dark" }>
     
-    {/* 버튼을 클릭하면 count의 값을 1로 변경해라
-     count = count + 1; count++; 에러난다.
-    */}
-      <button onClick={ () => {setCount(count + 1);}}>클릭하면 화면이 바뀌어지게</button>
-      <h2 style={{fontSize : 50}}>{count}</h2>
+    {/*버튼을 클릭하면 클래스명이 번갈아 가면서 들어온다.*/}
+      <button onClick={ () => {setToggle(!open);}}>클릭하면 화면이 바뀌어지게</button>
     </div>
   );
 }
